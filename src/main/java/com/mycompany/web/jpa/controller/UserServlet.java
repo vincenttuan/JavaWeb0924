096fab969bd2d2b30a209e1dd0d9fdca1685e696
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-12 19:24:27 +0800

    Java

--- src/main/java/com/mycompany/web/jpa/controller/UserServlet.java
+++ src/main/java/com/mycompany/web/jpa/controller/UserServlet.java
@@ -10,17 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // please use TomEE
-//@WebServlet("/jpa/controller/user/*")
+@WebServlet("/jpa_user/*")
 public class UserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        String pathInfo = req.getPathInfo();
+        if(pathInfo.equals("/user") || pathInfo.equals("/user/")) {
+            String user_name = req.getParameter("user_name");
+            Integer user_age = Integer.valueOf(req.getParameter("user_age"));
+            UserService service = new UserService();
+            boolean check = service.insert(user_name, user_age);
+            resp.getWriter().println("Insert: " + check);
+        } else {
+            resp.getWriter().println("Not allow !");
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        String pathInfo = req.getPathInfo();
+        resp.getWriter().println(pathInfo);
     }
 
 }
