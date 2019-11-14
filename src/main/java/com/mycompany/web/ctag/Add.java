0f0f3084e487755f616cd44d5879ed2f4a3596b5
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-14 20:12:17 +0800

    Java

---
+++ src/main/java/com/mycompany/web/ctag/Add.java
@@ -0,0 +1,57 @@
+package com.mycompany.web.ctag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.Tag;
+
+public class Add implements Tag {
+    private PageContext pageContext;
+    private Tag parentTag;
+    private int x, y;
+    
+    @Override
+    public void setPageContext(PageContext pc) {
+        pageContext = pc;
+    }
+
+    @Override
+    public void setParent(Tag tag) {
+        parentTag = tag;
+    }
+
+    @Override
+    public Tag getParent() {
+        return parentTag;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    @Override
+    public int doStartTag() throws JspException {
+        int z = x + y;
+        JspWriter out = pageContext.getOut();
+        try {
+            out.println(String.format("%d + %d = %d", x, y, z));
+        } catch (Exception e) {
+        }
+        return Tag.SKIP_BODY;
+    }
+
+    @Override
+    public int doEndTag() throws JspException {
+        return Tag.EVAL_PAGE;
+    }
+
+    @Override
+    public void release() {
+        
+    }
+    
+}
