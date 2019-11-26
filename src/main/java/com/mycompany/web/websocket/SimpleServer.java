7aeba97c689cc0bd44234403991af95e749f24c7
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-26 19:37:15 +0800

    Java

--- src/main/java/com/mycompany/web/websocket/SimpleServer.java
+++ src/main/java/com/mycompany/web/websocket/SimpleServer.java
@@ -14,7 +14,7 @@ public class SimpleServer {
 
     @OnOpen
     public void onOpen(Session session) {
-        
+        System.out.println("Server 接到連線, sessionID: " + session.getId());
     }
     
     @OnMessage
--- src/main/webapp/forms/websocket/SimpleClient.js
+++ src/main/webapp/forms/websocket/SimpleClient.js
@@ -1,6 +1,37 @@
 window.onload = function() {
     console.log('Play WebSocket');
+    var url = 'ws://' + window.location.hostname + ':' + window.location.port + '/JavaWeb0924/websocket/server';
+    var webSocket;
     
+    openBtn.addEventListener("click", function () {
+        setWebSocket();
+    });
+    
+    //設置 WebSocket
+    function setWebSocket() {
+        // 設定 websocket 物件
+        webSocket = new WebSocket(url);
+        
+        // onopen , 連線成功時觸發
+        webSocket.onopen = function (event) {
+            console.log('連線成功');
+            openBtn.disabled = true;
+        };
+
+        // onmessage , 接收到來自Server的訊息時觸發
+        webSocket.onmessage = function (event) {
+        };
+
+        // onclose , 連線關閉時觸發  
+        webSocket.onclose = function (event) {
+        };
+        
+        // onerror , 連線錯誤時觸發  
+        webSocket.onerror = function (event) {
+        };
+
+        
+    }
 }
 
 
