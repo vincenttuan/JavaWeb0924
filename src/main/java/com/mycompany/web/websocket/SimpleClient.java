620b496505d6bd122fd0a5426e4bb8cc85c44d10
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-26 21:13:24 +0800

    Java

--- src/main/java/com/mycompany/web/websocket/SimpleClient.java
+++ src/main/java/com/mycompany/web/websocket/SimpleClient.java
@@ -1,7 +1,30 @@
 package com.mycompany.web.websocket;
 
+import com.mycompany.web.websocket.client.console.SocketHandler;
+import java.net.URI;
+import java.util.Scanner;
+import javax.websocket.ContainerProvider;
+import javax.websocket.RemoteEndpoint;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
 public class SimpleClient {
-    public static void main(String[] args) {
-        System.out.println("Hello Client");
+public static void main(String[] args) throws Exception {
+        URI uri = new URI("ws://localhost:8080/JavaWeb0924/JavaWeb0924/websocket/server");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        Session session = container.connectToServer(new SocketHandler(), uri);
+        RemoteEndpoint.Async remote = session.getAsyncRemote();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("請輸入訊息: ");
+            String msg = scanner.nextLine();
+            if(msg.equalsIgnoreCase("exit")) {
+                break;
+            }
+            remote.sendText(msg);
+        }
+        
+        session.close();
+        
     }
 }
