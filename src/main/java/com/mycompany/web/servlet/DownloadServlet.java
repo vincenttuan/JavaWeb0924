12fab9bea74b9d1a48810186130bd1d43741f8e6
vincenttuan <vincentjava@yahoo.com.tw>
2019-11-26 09:27:28 +0800

    JavaWeb

--- src/main/java/com/mycompany/web/servlet/DownloadServlet.java
+++ src/main/java/com/mycompany/web/servlet/DownloadServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/servlet/download")
-public class DownloadFileServlet extends HttpServlet {
+public class DownloadServlet extends HttpServlet {
  
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
