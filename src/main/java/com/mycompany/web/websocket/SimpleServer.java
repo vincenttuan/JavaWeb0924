4fd46dd415e7ac4d24f62846dc4c88eabb82fa51
MB-Teacher <MB-Teacher@MB-207-teacher>
2019-11-26 19:06:43 +0800

    Java

---
+++ src/main/java/com/mycompany/web/websocket/SimpleServer.java
@@ -0,0 +1,35 @@
+package com.mycompany.web.websocket;
+
+import java.io.IOException;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/websocket/server")
+public class SimpleServer {
+
+    @OnOpen
+    public void onOpen(Session session) {
+        
+    }
+    
+    @OnMessage
+    public void onMessage(String message, Session session) throws Exception {
+        
+    }
+    
+    @OnClose
+    public void onClose(Session session) {
+        
+    }
+    
+    @OnError
+    public void onError(Throwable error) {
+        
+    }
+    
+}
